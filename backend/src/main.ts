20e959eebc48354cc3d4163b5094895eb8472304
Nikola Uzunov <nikollla2000@abv.bg>
2024-02-21 16:22:54 +0200

    Added dotenv and changed the port to 5000 from the .env file

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,14 @@
+import { config } from 'dotenv';
+config()
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT;
+  
+  await app.listen(port);
 }
+
 bootstrap();
