86d14666a9a58afd0e4d146a32d334abc332b8dc
Nikola Uzunov <nikollla2000@abv.bg>
2024-02-25 17:21:54 +0200

    Created some placeholder data for the polls

---
+++ frontend/src/lib/placeholder-data.js
@@ -0,0 +1,22 @@
+export const votingPolls = [
+  {
+    title: 'Lorem',
+    description: 'lorem wefpokwe powkef powekp ofwepok wepkwe',
+    image: '',
+  },
+  {
+    title: 'Lorem',
+    description: 'lorem wefpokwe powkef powekp ofwepok wepkwe',
+    image: '',
+  },
+  {
+    title: 'Lorem',
+    description: 'lorem wefpokwe powkef powekp ofwepok wepkwe',
+    image: '',
+  },
+  {
+    title: 'Lorem',
+    description: 'lorem wefpokwe powkef powekp ofwepok wepkwe',
+    image: '',
+  },
+]
