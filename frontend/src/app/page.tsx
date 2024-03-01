4701b81f48366ea14e91327e2c321b85d909f004
Nikola Uzunov <nikollla2000@abv.bg>
2024-03-01 09:53:40 +0200

    fix: hydration error in the main page
    hydration error was caused due to nesting a div in a link (a) tag

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -13,18 +13,16 @@ export default function Home() {
         <h3 className='text-purple-700 text-center text-xl'>The Future of the Web</h3>
       </div>
       <div>
-        <Link href="/login">
         <div className="relative inline-flex  group mt-10">
           <div
               className="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt">
           </div>
-          <a href="#" title="Get quote now"
+        <Link href="/login" title='Get quote now'
               className="relative inline-flex items-center justify-center px-8 py-4 text-lg font-bold text-white transition-all duration-200 bg-blue-900 font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
-              role="button">Sign in To Vote
-          </a>
+              role="button">Sign in To Vote     
+        </Link>
         </div>
         {/* <button type="button" className="mt-10 text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Sign In To Vote</button> */}
-        </Link>
       </div>
       <Link 
           href='/register'
