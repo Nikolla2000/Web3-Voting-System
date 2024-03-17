5a85e1d5a5ae7c606e1846bfc67fc78bf7bafd16
Nikola Uzunov <nikollla2000@abv.bg>
2024-03-17 18:51:55 +0200

    feat: write second version of a create user function in the user service
    This time using bcrypt, and user dto

--- backend/src/users/user.service.ts
+++ backend/src/users/user.service.ts
@@ -1,6 +1,7 @@
-import { BadRequestException, Injectable } from "@nestjs/common";
+import { BadRequestException, ConflictException, Injectable } from "@nestjs/common";
 import { Prisma, User } from "@prisma/client";
 import { PrismaService } from "src/prisma.service";
+import { CreateUserDto } from "./dto/user.dto";
 
 @Injectable()
 export class UserSevice {
@@ -15,11 +16,28 @@ export class UserSevice {
     return this.prisma.user.findUnique({ where: {id:Number(id)}})
   }
 
-  async createUser(data: Prisma.UserCreateInput): Promise<User> {
-    try {
-      return await this.prisma.user.create({ data })
-    } catch (error) {
-      throw new BadRequestException('Could not create user')
-    }
+  // async createUser(data: Prisma.UserCreateInput): Promise<User> {
+  //   try {
+  //     return await this.prisma.user.create({ data })
+  //   } catch (error) {
+  //     throw new BadRequestException('Could not create user')
+  //   }
+  // }
+
+  async createTestTwo(dto: CreateUserDto): Promise<User> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      }
+    });
+
+    if(user) throw new ConflictException("email already exists")
+
+    const newUser = await this.prisma.user.create({
+      data: {
+        ...dto
+      }
+    })
+    return newUser;
   }
 }
