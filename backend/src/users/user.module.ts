eb382bc3be473ebcc531fe4530dd7c0a1a13a2fb
Nikola Uzunov <nikollla2000@abv.bg>
2024-03-18 07:58:49 +0200

    feat: add JwtGuard

--- backend/src/users/user.module.ts
+++ backend/src/users/user.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { UserController } from "./user.controller";
 import { UserSevice } from "./user.service";
 import { PrismaService } from "src/prisma.service";
+import { JwtService } from "@nestjs/jwt";
 
 @Module({
   // imports: [],
   controllers: [UserController],
-  providers: [UserSevice, PrismaService]
+  providers: [UserSevice, PrismaService, JwtService]
 })
 export class UserModule {}
