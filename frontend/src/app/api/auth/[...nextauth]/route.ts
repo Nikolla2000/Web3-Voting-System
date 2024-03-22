edc7ac76036e350b39af24d1a5cfcbfe14c9ca96
Nikola Uzunov <nikollla2000@abv.bg>
2024-03-22 17:23:09 +0200

    feat: add authOptions, installed next-auth

---
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
+import Credentials, { CredentialsProvider } from "next-auth/providers/credentials";
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        Credentials({
+            name: 'Credentials',
+            credentials: {
+                email: {
+                    label: 'Email',
+                    type: 'text',
+                    placeholder: 'jsmith',
+                },
+                password: { label: 'Password', type: 'password' },
+            },
+            async authorize(credentials, req) {
+                if (!credentials?.email || !credentials?.password) return null;
+                const { email, password } = credentials;
+                try {
+                    const res = await axios.post('localhost:5000/auth/login', {
+                        email,
+                        password
+                    });
+                    const user = res.data;
+                    return user;
+
+                } catch (error) {
+                    console.error("Login failed: ", error);
+                }
+            }
+        })
+    ]
+}
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
