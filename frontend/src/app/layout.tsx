8f56c081ecc56bf7b0483059e6e80a102793234a
Nikola Uzunov <nikollla2000@abv.bg>
2024-03-23 22:52:08 +0200

    feat: Providers
    Wrapped the whole next app in the "Providers" component so that all the client components have access to the use-session hook.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Providers from "./ui/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,10 +21,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} min-h-screen `}>{children}
-      {/* <img src="/network3.png" className="network-bg-image"/>
-      <img src="/network3.png" className="network-bg-image network-bg-image2"/>
-      <img src="/network3.png" className="network-bg-image network-bg-image3"/> */}
+      <body className={`${inter.className} min-h-screen `}>
+        <Providers>
+          {children}
+      </Providers>
       </body>
     </html>
   );
