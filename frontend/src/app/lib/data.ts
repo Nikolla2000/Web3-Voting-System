c90ebf49f4c233fb6a523590524bd1c6691b6b1a
Nikola Uzunov <nikollla2000@abv.bg>
2024-03-24 23:20:00 +0200

    feat: Implement registration page
    Add registration form with input fields for email, name, password, confirm password, and country dropdown
    Integrate react-hook-form for form validation and submission handling
    Populate the country dropdown with a list of countries fetched from utils/countries

--- frontend/src/app/lib/data.ts
+++ frontend/src/app/lib/data.ts
@@ -1,5 +1,8 @@
 import { unstable_noStore as noStore } from 'next/cache';
 import { votingPolls } from './placeholder-data'
+import axios from 'axios';
+import { Inputs } from '../ui/login/register-form';
+import { toast } from "react-hot-toast"
 
 export async function fetchPollsData() {
   try {
@@ -18,4 +21,13 @@ export async function fetchPollById(id: number) {
   } catch (error) {
     console.error('Error: ', error);
   }
+}
+
+export async function fetchRegister(data: Inputs) {
+  if(data.confirmPass != data.password) {
+    console.log(data);
+    toast.error("Passwords do not match");
+    return;
+  }
+  // axios.post('http://localhost:5000/user/regiseter', data);
 }
