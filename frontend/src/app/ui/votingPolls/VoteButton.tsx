d1f0ccc11d07ffc7ddad5a98a0954aa362e423ff
Nikola Uzunov <109785756+Nikolla2000@users.noreply.github.com>
2024-04-11 17:41:16 +0300

    feat: vote button animation
    Now the entire option image in the poll page acts as a button which opens the vote confirmation modal. When you hover on the option image the vote button has a little animation too.

--- frontend/src/app/ui/votingPolls/VoteButton.tsx
+++ frontend/src/app/ui/votingPolls/VoteButton.tsx
@@ -18,7 +18,7 @@ export function VoteButton({ isHovered } : {isHovered: boolean}) {
     }
     return (
         <div className={`absolute ${!isHovered && 'hidden'} z-10 ${styles.voteBtn}`}>
-            <button onClick={handleVote} className="z-20 bg-transparent text-white text-3xl font-bold uppercase border-2 py-4 px-10">
+            <button className="z-20 bg-transparent  text-3xl font-bold uppercase py-4 px-10">
                 Vote
             </button>
         </div>
--- frontend/src/app/votingPolls/[id]/PollImage.tsx
+++ frontend/src/app/votingPolls/[id]/PollImage.tsx
@@ -3,16 +3,25 @@
 import { VoteButton } from "@/app/ui/votingPolls/VoteButton"
 import styles from '../../ui/votingPolls/styles.module.css';
 import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 import VoteModal from "./VoteModal";
-
 
 export default function PollImage({ pollImg } : { pollImg: string}){
     const [isHovered, setIsHovered] = useState(false);
     const [showModal, setShowModal] = useState(false);
 
-    const handleClick = () => {
-        setShowModal(true);
-        setIsHovered(false);
+    const { data: session } = useSession();
+    const router = useRouter();
+
+
+    const openVoteModal = () => {
+        if(!session || !session?.user) {
+            router.push('/api/auth/signin')
+        } else {
+            setShowModal(true);
+            setIsHovered(false);
+        }
     }
 
     return (
@@ -21,7 +30,7 @@ export default function PollImage({ pollImg } : { pollImg: string}){
                 className={`relative ${styles.boxShadow} rounded-lg`} 
                 onMouseEnter={() => setIsHovered(true)}
                 onMouseLeave={() => setIsHovered(false)}
-                onClick={handleClick}>
+                onClick={openVoteModal}>
                     <img src={pollImg} alt="Poll image" className={styles.images}/>
                     <VoteButton isHovered={isHovered}/>
                     <div className={`${isHovered && styles.darkOverlay} cursor-pointer`}></div>
