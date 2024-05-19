cf1f8cf401742f16ae0be0b2849012be4cccdade
Nikola Uzunov <nikollla2000@abv.bg>
2024-05-19 17:12:43 +0300

    feat: Users can now vote only if they are connected to metamask

--- frontend/src/app/ui/votingPolls/VoteButton.tsx
+++ frontend/src/app/ui/votingPolls/VoteButton.tsx
@@ -1,27 +1,42 @@
 'use client'
 
-import { vote } from "@/app/lib/actions";
+import { connectMetamask, vote } from "@/app/lib/actions";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import styles from "./styles.module.css"
 import { Button } from "react-bootstrap";
 import { ChosenOption, Poll } from "@/app/_types/types";
+import { useMetamask } from "../MetamaskProvider";
+import toast from "react-hot-toast";
+import MetamaskButton from "../metamask/MetamaskButton";
+import { useState } from "react";
 
 export function VoteButton({ chosenOption, poll } : { chosenOption: ChosenOption, poll: Poll}) {
     const { data: session } = useSession();
+    const { connectedAccount, setConnectedAccount } = useMetamask();
+    const [showMetamaskBtn, setShowMetamaskBtn] = useState<boolean>(false)
     const router = useRouter();
 
     const handleVote = () => {
         if(!session || !session?.user) {
             router.push('/api/auth/signin')
-        } else {
+        } else if(!connectedAccount) {
+            toast.error("You need to connect to Metamask to vote.")
+            setShowMetamaskBtn(true);
+        }else {
             vote(chosenOption, poll.id, session?.user.id);
+            window.location.reload();
         }
-        window.location.reload();
     }
     return (
-        <Button variant="primary" onClick={handleVote}>
-            {`Vote for ${chosenOption == ChosenOption.First ? poll.optionOneName : poll.optionTwoName}`}
-        </Button>
+        <>
+            {showMetamaskBtn && <p className="text-blue-600 font-bold">Connect to Metamask from the button below</p>}
+            <div className="flex flex-row justify-center gap-2">
+                <Button variant="primary" onClick={handleVote}>
+                    {`Vote for ${chosenOption == ChosenOption.First ? poll.optionOneName : poll.optionTwoName}`}
+                </Button>
+                {showMetamaskBtn && <MetamaskButton/>}
+            </div>
+        </>
     )
 }
