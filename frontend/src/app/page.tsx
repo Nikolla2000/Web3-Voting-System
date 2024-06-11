7425fc8cbf6342347b9d8aabf79bbd2b0455c98e
Nikola Uzunov <109785756+Nikolla2000@users.noreply.github.com>
2024-06-11 09:48:01 +0300

    style
    - Added spinning geometrical shapes to the main page, but will be commented for now and be saved for later use
    - Changed the background to the polls page to purple-white gradient

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,12 @@
 import 'animate.css';
-import Link from 'next/link';
 import SignInButton from './ui/login/SignInButton';
 import RegisterButton from './ui/login/RegisterButton';
 import MetamaskButton from './ui/metamask/MetamaskButton';
 import ViewPollsButton from './ui/votingPolls/ViewPollsButton';
 import MetamaskAccount from './ui/metamask/MetamaskAccount';
 import TypewriterEffect from './ui/typeWriterEffects/MainPageTpEffect';
+import { shapesData } from '../../utils/shapesData';
+import "./ui/shapes.css";
 
 
 export default function Home() {
@@ -19,8 +20,10 @@ export default function Home() {
     </video>
       </div>
       <div>
-        <h1 className="text-blue-700 text-5xl font-bold mb-6 text-center">Welcome to Web3</h1>
-        <h3 className='text-purple-700 text-center text-xl h-6'>
+        <h1 className="text-blue-700 text-5xl font-bold mb-6 text-center">
+          Welcome to Web3
+        </h1>
+        <h3 className='text-purple-700 text-center text-2xl h-6'>
           <TypewriterEffect/>
         </h3>
       </div>
@@ -37,14 +40,15 @@ export default function Home() {
         <MetamaskButton/>
 
         </div>
-        {/* <button type="button" className="mt-10 text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Sign In To Vote</button> */}
       </div>
       <RegisterButton/>
-      {/* <div className='w-5 h-5 bg-red-400 absolute top-50 shadow-md left-10 rounded-xl'></div>
-      <div className='w-5 h-5 bg-red-400 absolute top-20 shadow-md right-10 rounded-xl'></div>
-      <div className='w-5 h-5 bg-red-400 absolute top-30 shadow-md left-50 rounded-xl'></div>
-      <div className='w-5 h-5 bg-red-400 absolute top-50 shadow-md left-40 rounded-xl'></div>
-      <div className='w-5 h-5 bg-red-400 absolute top-70 shadow-md left-10 rounded-xl'></div> */}
+      {/* <div>
+        {shapesData.map((shape, i) => (
+            <div className={`shape shape${i + 1}`} key={i + 1}>
+              <img src={shape.imgSrc}  alt="Geometrical shape"/>
+            </div>
+        ))}
+      </div> */}
     </main>
   );
 }
