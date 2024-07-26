26d605513e49dd13fed1efd7ce18844aa4daab1a
Nikola Uzunov <109785756+Nikolla2000@users.noreply.github.com>
2024-07-26 15:47:06 +0300

    Wrapping useSearchParams in a Suspense

--- frontend/src/app/transaction/page.tsx
+++ frontend/src/app/transaction/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import CongratMessage from "./CongratMessage";
 import TransactionHash from "./TransactionHash";
 import styles from './styles.module.css';
@@ -5,8 +6,12 @@ import styles from './styles.module.css';
 export default async function TransactionHashPage(){
     return (
         <div className="text-center py-10">
-            <CongratMessage/>
-            <TransactionHash/>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <CongratMessage />
+                </Suspense>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <TransactionHash/>
+                </Suspense>
             <img src="/tesseract.gif" alt="Spinning tesseract"  className={`${styles.tesseract} hidden xl:block absolute w-96 top-1/4 right-28 opacity-35`}/>
             <img src="/tesseract.gif" alt="Spinning tesseract" className="hidden xl:block absolute w-96 top-1/4 left-28 opacity-35"/>
         </div>
