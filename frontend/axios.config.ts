facf843db74c0fe5caf85c972f26cf2f88b53bd2
Nikola Uzunov <nikollla2000@abv.bg>
2024-07-28 17:43:08 +0300

    Point to production api

--- frontend/axios.config.ts
+++ frontend/axios.config.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000', //local server url
-  // baseURL: 'https://web3-voting-system.onrender.com', //production server url
+  // baseURL: 'http://localhost:5000', //local server url
+  baseURL: 'https://web3-voting-system.onrender.com', //production server url
 })
 
 export default api;
