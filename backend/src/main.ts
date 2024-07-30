6330330372ad86a717578d47321463907905f401
Nikola Uzunov <109785756+Nikolla2000@users.noreply.github.com>
2024-07-30 14:06:22 +0300

    enable cors only for production website url

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT;
   
-  app.enableCors();
+  app.enableCors({
+    // origin: 'http://localhost:3000/', //local origin
+    origin: 'https://web3-voting-system-mu.vercel.app/', //production url origin
+  });
   
   await app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
