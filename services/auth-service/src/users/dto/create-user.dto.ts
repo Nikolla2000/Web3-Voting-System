8301386651d990d16894bf28f84b3fdce59ccd05
Nikola Uzunov <109785756+Nikolla2000@users.noreply.github.com>
2026-06-07 16:13:50 +0300

    create user dtos
    - create-user dto
    - update user-dto

---
+++ services/auth-service/src/users/dto/create-user.dto.ts
@@ -0,0 +1,29 @@
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
+import { IsEmail, IsOptional, IsString, MinLength } from "class-validator";
+
+export class CreateUserDto {
+    @ApiProperty({ example: 'some_user' })
+    @IsString()
+    @MinLength(3)
+    username: string;
+
+    @ApiProperty({ example: 'some_user@gmail.com' })
+    @IsEmail()
+    email: string;
+
+    @ApiPropertyOptional()
+    @IsString()
+    @MinLength(8)
+    @IsOptional()
+    password?: string;
+
+    @ApiPropertyOptional()
+    @IsString()
+    @IsOptional()
+    avatar?: string;
+
+    @ApiPropertyOptional()
+    @IsString()
+    @IsOptional()
+    walletAddress?: string;
+}
