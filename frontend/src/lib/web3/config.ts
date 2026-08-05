64dcd9f6531f4601e7c38e1f1cabc0ca3e5dbe3f
Nikola Uzunov <109785756+Nikolla2000@users.noreply.github.com>
2026-08-05 15:32:24 +0300

    improve wagmi config

--- frontend/src/lib/web3/config.ts
+++ frontend/src/lib/web3/config.ts
@@ -1,9 +1,30 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import { sepolia } from 'wagmi/chains';
+import {
+  metaMaskWallet,
+  rainbowWallet,
+  walletConnectWallet,
+} from '@rainbow-me/rainbowkit/wallets';
+
+const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID;
+
+if (!projectId) {
+  throw new Error('NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID is missing');
+}
 
 export const wagmiConfig = getDefaultConfig({
   appName: 'VoteChain',
-  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID ?? '',
+  projectId,
   chains: [sepolia],
+   wallets: [
+    {
+      groupName: 'Recommended',
+      wallets: [
+        metaMaskWallet,
+        walletConnectWallet,
+        rainbowWallet,
+      ],
+    },
+  ],
   ssr: true,
 })
