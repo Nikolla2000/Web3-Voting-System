856806cd3365baf70582bf523a72d0bcec0fa817
Nikola Uzunov <109785756+Nikolla2000@users.noreply.github.com>
2026-09-06 21:41:01 +0300

    register config module in polls service

--- backend/apps/polls/src/polls.module.ts
+++ backend/apps/polls/src/polls.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { PollsController } from './polls.controller';
 import { PollsService } from './polls.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: 'apps/polls/.env',
+      isGlobal: true,
+      load: [],
+      validationOptions: {
+        allowUnknown: true,
+        abortEarly: true
+      }
+    }),
     PrismaModule
   ],
   controllers: [PollsController],
